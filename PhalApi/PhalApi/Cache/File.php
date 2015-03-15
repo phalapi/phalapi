610dbfb460107cd795d85dfadcc5780efffbca79
dogstar <chanzonghuang@gmail.com>
2015-03-15 15:22:36 +0800

    support huge cache file

--- PhalApi/PhalApi/Cache/File.php
+++ PhalApi/PhalApi/Cache/File.php
@@ -69,8 +69,17 @@ class PhalApi_Cache_File implements PhalApi_Cache {
         @unlink($filePath);
     }
 
+	/**
+	 * 考虑到Linux同一目录下的文件个数限制，这里拆分成1000个文件缓存目录
+	 */
     protected function createCacheFilePath($key) {
-        return $this->createCacheFileFolder() . DIRECTORY_SEPARATOR . md5($key) . '.dat';
+        $folderSufix = sprintf('%03d', hexdec(substr(sha1($key), -5)) % 1000);
+        $cacheFolder = $this->createCacheFileFolder() . DIRECTORY_SEPARATOR . $folderSufix;
+        if (!is_dir($cacheFolder)) {
+            mkdir($cacheFolder, 0777, TRUE);
+        }
+
+        return $cacheFolder . DIRECTORY_SEPARATOR . md5($key) . '.dat';
     }
 
     protected function createCacheFileFolder() {
