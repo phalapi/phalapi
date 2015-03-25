991528adb67d5461e8c1e7100353f1f3417557e0
dogstar <chanzonghuang@gmail.com>
2015-03-25 22:01:37 +0800

    docs continue

--- PhalApi/PhalApi/Config.php
+++ PhalApi/PhalApi/Config.php
@@ -4,6 +4,7 @@
  *
  * 获取系统所需要的参数配置
  *
+ * @package PhalApi\Config
  * @author dogstar <chanzonghuang@gmail.com> 2014-10-02
  */
 
@@ -11,8 +12,10 @@ interface PhalApi_Config {
 
 	/**
      * 获取配置
+     * 
      * @param $key string 配置键值
-     * @return mixed 需要获取的配置值
+     * @param mixed $default 缺省值
+     * @return mixed 需要获取的配置值，不存在时统一返回$default
      */
 	public function get($key, $default = NULL);
 }
--- PhalApi/PhalApi/Config/File.php
+++ PhalApi/PhalApi/Config/File.php
@@ -2,18 +2,30 @@
 /**
  * PhalApi_Config_File 文件配置类
  *
- * - 从配置文件获取参数配置
+ * <li>从配置文件获取参数配置</li>
  * 
  * 使用示例：
+ * <br>
+ * <code>
  *      $config = new PhalApi_Config_File('./Config');
  *      $config->get('sys.db.user');
+ * </code>
  *
+ * @package PhalApi\Config
+ * @see PhalApi_Config::get()
  * @author dogstar <chanzonghuang@gmail.com> 2014-10-02
  */
 
 class PhalApi_Config_File implements PhalApi_Config {
 
+	/**
+	 * @var string $path 配置文件的目录位置
+	 */
 	private $path = '';
+	
+	/**
+	 * @var array $map 配置文件的映射表，避免重复加载 
+	 */
 	private $map = NULL;
 	
 	public function __construct($configPath) {
@@ -26,7 +38,6 @@ class PhalApi_Config_File implements PhalApi_Config {
      *
      * @param $key string 配置键值
      * @return mixed 需要获取的配置值
-     * @author dogstar 2012-12-23
      */
 	public function get($key, $default = NULL) {
 		$keyArr = explode('.', $key);
@@ -55,8 +66,7 @@ class PhalApi_Config_File implements PhalApi_Config {
      * 加载保存配置信息数组的config.php文件，若文件不存在，则将$map置为空数组
      *
      * @param string $fileName 配置文件路径
-     * @return 
-     * @author	dogstar 2012-12-23
+     * @return array 配置文件对应的内容
      */
 	private function loadConfig($fileName) {
 		$config = include($this->path . DIRECTORY_SEPARATOR . $fileName . '.php');
