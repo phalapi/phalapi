4ddc0c5af8a0fc3fa23fd1452b84c43e22d4f939
dogstar <chanzonghuang@gmail.com>
2015-03-25 21:50:55 +0800

    docs continue

--- PhalApi/PhalApi/Cache.php
+++ PhalApi/PhalApi/Cache.php
@@ -7,12 +7,27 @@
 
 interface PhalApi_Cache {
 
+	/**
+	 * 设置缓存
+	 * 
+	 * @param string $key 缓存key
+	 * @param mixed $value 缓存的内容
+	 * @param int $expire 缓存有效时间，单位秒，非时间戳
+	 */
     public function set($key, $value, $expire = 600);
 
     /**
-    * @return mixed/NULL 失败情况下返回NULL
-    */
+     * 读取缓存
+     * 
+     * @param string $key 缓存key
+     * @return mixed 失败情况下返回NULL
+     */
     public function get($key);
 
+    /**
+     * 删除缓存
+     * 
+     * @param string $key
+     */
     public function delete($key);
 }
--- PhalApi/PhalApi/Crypt.php
+++ PhalApi/PhalApi/Crypt.php
@@ -1,13 +1,24 @@
 <?php
 /**
- * 对称加密
+ * PhalApi_Crypt对称加密接口
  *
  * @author dogstar <chanzonghuang@gmail.com> 2014-12-10
  */
 
 interface PhalApi_Crypt {
 
+	/**
+	 * 对称加密
+	 * 
+	 * @param mixed $data 等加密的数据
+	 * @param string $key 加密的key
+	 */
     public function encrypt($data, $key);
     
+    /**
+     * 对称解密
+     * @param mixed $data 对称加密后的内容{@see PhalApi_Crypt::encrypt()}
+     * @param string $key 加密的key
+     */
     public function decrypt($data, $key);
 }
