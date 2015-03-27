7512373c951a12b1c195321e0e0b53414016d883
dogstar <chanzonghuang@gmail.com>
2015-03-27 22:46:36 +0800

    docs update

--- PhalApi/PhalApi/ModelProxy.php
+++ PhalApi/PhalApi/ModelProxy.php
@@ -11,19 +11,19 @@
 ```
  * class ModelProxy_UserBaseInfo extends PhalApi_ModelProxy {
  *
- *	protected function doGetData($query) {
- *		$model = new Model_User();
- *
- *		return $model->getByUserId($query->id);
- *	}
- *
- *	protected function getKey($query) {
- *		return 'userbaseinfo_' . $query->id;
- *	}
- *
- *	protected function getExpire($query) {
- *		return 600;
- *	}
+ *      protected function doGetData($query) {
+ *      	$model = new Model_User();
+ *      
+ *      	return $model->getByUserId($query->id);
+ *      }
+ *     
+ *      protected function getKey($query) {
+ *      	return 'userbaseinfo_' . $query->id;
+ *      }
+ *     
+ *      protected function getExpire($query) {
+ *      	return 600;
+ *      }
  * }
  * 
  * //最终的调用
