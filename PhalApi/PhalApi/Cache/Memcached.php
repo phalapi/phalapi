e4bf890196b0650d9fbd705e9e031df51a7bde44
dogstar <chanzonghuang@gmail.com>
2015-05-07 22:36:49 +0800

    bugfixed

--- PhalApi/PhalApi/Cache/Memcached.php
+++ PhalApi/PhalApi/Cache/Memcached.php
@@ -12,9 +12,9 @@
 
 class PhalApi_Cache_Memcached extends PhalApi_Cache_Memcache {
 
-	/**
-	 * 注意参数的微妙区别
-	 */
+    /**
+     * 注意参数的微妙区别
+     */
     public function set($key, $value, $expire = 600) {
         $this->memcache->set($this->formatKey($key), @serialize($value), $expire);
     }
