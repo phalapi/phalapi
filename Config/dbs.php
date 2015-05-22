a6b707505330c22f38136f603d613b081f35b82b
dogstar <chanzonghuang@gmail.com>
2015-05-22 20:40:08 +0800

    phalapi 1.2.0

--- Config/dbs.php
+++ Config/dbs.php
@@ -10,21 +10,13 @@ return array(
      * DB数据库服务器集群
      */
     'servers' => array(
-        /**
         'db_demo' => array(
             'host'      => 'localhost',             //数据库域名
-            'name'      => 'test',                  //数据库名字
+            'name'      => 'phalapi',               //数据库名字
             'user'      => 'root',                  //数据库用户名
-            'password'  => '123456',	            //数据库密码
+            'password'  => '',	                    //数据库密码
             'port'      => '3306',		            //数据库端口
-        ),
-         */
-        'db_demo' => array(
-            'host'      => '112.74.107.125',         //数据库域名
-            'name'      => 'phalapi_test',          //数据库名字
-            'user'      => 'test',                  //数据库用户名
-            'password'  => '123456',	            //数据库密码
-            'port'      => '3306',		            //数据库端口
+            'charset'   => 'UTF8',                  //数据库字符集
         ),
     ),
 
@@ -32,6 +24,7 @@ return array(
      * 自定义路由表
      */
     'tables' => array(
+        //通用路由
         '__default__' => array(
             'prefix' => 'tbl_',
             'key' => 'id',
@@ -39,6 +32,7 @@ return array(
                 array('db' => 'db_demo'),
             ),
         ),
+
         /**
         'demo' => array(
             'prefix' => 'tbl_',
