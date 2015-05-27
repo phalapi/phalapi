792116a9829cd855f14fd5b1bc469a73d71a6c22
dogstar <chanzonghuang@gmail.com>
2015-05-27 23:09:44 +0800

    no blank

--- Demo/Api/User.php
+++ Demo/Api/User.php
@@ -45,9 +45,9 @@ class Api_User extends PhalApi_Api {
      * 批量获取用户基本信息
      * @return int code 操作码，0表示成功
      * @return array list 用户列表
-     * @return int list[ ].id 用户ID
-     * @return string list[ ].name 用户名字
-     * @return string list[ ].from 用户来源
+     * @return int list[].id 用户ID
+     * @return string list[].name 用户名字
+     * @return string list[].from 用户来源
      * @return string msg 提示信息
      */
     public function getMultiBaseInfo() {
