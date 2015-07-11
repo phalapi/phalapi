fe4dc08dd05749dd53daac1452d1b7dbe486875c
dogstar <chanzonghuang@gmail.com>
2015-07-11 10:42:00 +0800

    tag

--- Config/dbs.php
+++ Config/dbs.php
@@ -15,7 +15,7 @@ return array(
             'name'      => 'phalapi',               //数据库名字
             'user'      => 'root',                  //数据库用户名
             'password'  => '',	                    //数据库密码
-            'port'      => '3306',		            //数据库端口
+            'port'      => '3306',                  //数据库端口
             'charset'   => 'UTF8',                  //数据库字符集
         ),
     ),
@@ -34,12 +34,12 @@ return array(
         ),
 
         /**
-        'demo' => array(												//表名
-            'prefix' => 'tbl_',											//表名前缀
-            'key' => 'id',												//表主键名
-            'map' => array(												//表路由配置
-                array('db' => 'db_demo'),								//单表配置：array('db' => 服务器标记)
-                array('start' => 0, 'end' => 2, 'db' => 'db_demo'),		//分表配置：array('start' => 开始下标, 'end' => 结束下标, 'db' => 服务器标记)
+        'demo' => array(                                                //表名
+            'prefix' => 'tbl_',                                         //表名前缀
+            'key' => 'id',                                              //表主键名
+            'map' => array(                                             //表路由配置
+                array('db' => 'db_demo'),                               //单表配置：array('db' => 服务器标记)
+                array('start' => 0, 'end' => 2, 'db' => 'db_demo'),     //分表配置：array('start' => 开始下标, 'end' => 结束下标, 'db' => 服务器标记)
             ),
         ),
          */
