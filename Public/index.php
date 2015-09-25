20cf8ec2ef4bd613075c47c194d0b762e1bdb20d
dogstar <chanzonghuang@gmail.com>
2015-09-25 20:36:44 +0800

    bugfixed

--- Public/index.php
+++ Public/index.php
@@ -6,7 +6,7 @@
 require_once dirname(__FILE__) . '/init.php';
 
 //装载你的接口
-DI()->loader->addDirs('Demo');
+DI()->loader->addDirs(array('Demo', 'Library'));
 
 /** ---------------- 响应接口请求 ---------------- **/
 
