e0503f892e5810df7c45b6f7219e510dbd2781cb
zhaoxiang <756958008@qq.com>
2015-10-15 22:52:53 +0800

    自动生成接口列表

--- Public/demo/listAllApis.php
+++ Public/demo/listAllApis.php
@@ -1,52 +1,63 @@
+<?php
+$root = dirname(__FILE__);
+
+/**
+ * 项目的文件夹名
+ */
+$apiDirName = 'Demo';
+/**
+ * 不参与到接口文档中的文件名 例：['.','..','Test.php']那么Test.php中的方法将不被列出
+ * 注：. 和 .. 是必须得有的！！！！！
+ */
+$prevention = ['.','..'];
+
+require_once $root . '/../init.php';
+DI()->loader->addDirs([$apiDirName]);
+$files = scandir($root.'/../../'.$apiDirName.'/Api');
+$files = array_diff($files, $prevention);
+foreach( $files as $value ){
+    $ApiServer = rtrim($value,'.php');
+    $Method = array_diff(get_class_methods('Api_'.$ApiServer), get_class_methods('PhalApi_Api'));
+    foreach( $Method as $MValue ){
+        $rMethod = new ReflectionMethod('Api_'.$ApiServer, $MValue);
+        $docComment = $rMethod->getDocComment();
+        $docCommentArr = explode("\n", $docComment);
+        $comment = trim($docCommentArr[1]);
+        $desc = substr($comment, strpos($comment, '*') + 1);
+        $description[$ApiServer.'.'.$MValue] = $desc;
+        $ApiS[] = $ApiServer.'.'.$MValue;
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Demo - 公共接口大全</title>
-
-    <!-- 新 Bootstrap 核心 CSS 文件 -->
+    <title>freeApi - 接口列表</title>
     <link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.2.0/css/bootstrap.min.css">
-
-    <!-- Bootstrap -->
-    <!-- <link href="/css/bootstrap.min.css" rel="stylesheet"> -->
 </head>
-
 <body>
-
-<br /> 
-
+<br />
 <div class="container">
-
-<div class="page-header">
-    <h1>用户模块</h1>
-</div>
-<table class="table table-hover">
-    <thead>
+    <div class="page-header">
+        <h1>用户模块</h1>
+    </div>
+    <table class="table table-hover">
+        <thead>
         <tr>
             <th>#</th><th>接口服务</th><th>接口名称</th><th>更多说明</th>
         </tr>
-    </thead>
-
-    <tbody>
-        <!--
-        <tr>
-            <td></td>
-            <td><a href="/fami/checkApiParams.php?service=" target="_blank">XXX.XXX</a></td>
-            <td></td>
-            <td></td>
-        </tr>
-        -->
-        <tr>
-            <td>1</td>
-            <td><a href="/demo/checkApiParams.php?service=User.GetBaseInfo" target="_blank">User.GetBaseInfo</a></td>
-            <td>获取用户信息</td>
-            <td></td>
-        </tr>
-    </tbody>
-</table>
-
-
-</div> <!-- /container -->
+        </thead>
+        <tbody>
+        <?php
+        foreach( $ApiS as $KK => $VV ){
+            $NO = $KK + 1;
+            echo "<tr><td>{$NO}</td><td><a href='checkApiParams.php?service={$VV}' target='_blank'>{$VV}</a></td><td>{$description[$VV]}</td><td></td></tr>";
+        }
+        ?>
+        </tbody>
+    </table>
+</div>
 
 </body>
 </html>
