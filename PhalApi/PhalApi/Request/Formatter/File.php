2d42e7929037e90ce78e91f6879dd6cbf8bca6a1
文振熙 <wenzhenxi@vip.qq.com>
2016-01-01 11:00:47 +0800

    2016/1/1 提交

--- PhalApi/PhalApi/Request/Formatter/File.php
+++ PhalApi/PhalApi/Request/Formatter/File.php
@@ -16,7 +16,7 @@ class PhalApi_Request_Formatter_File extends PhalApi_Request_Formatter_Base impl
      *
      * @throws PhalApi_Exception_BadRequest
      */
-    public function parse ($value, $rule) {
+    public function parse($value, $rule) {
 
         $default = isset($rule['default']) ? $rule['default'] : NULL;
 
@@ -25,36 +25,36 @@ class PhalApi_Request_Formatter_File extends PhalApi_Request_Formatter_Base impl
             return $default;
         }
 
-        if (!isset($_FILES[$index]) || !isset($_FILES[$index]['error']) || is_array ($_FILES[$index]['error'])) {
-            throw new PhalApi_Exception_BadRequest(T ('miss upload file: {file}', array ('file' => $index)));
+        if (!isset($_FILES[$index]) || !isset($_FILES[$index]['error']) || is_array($_FILES[$index]['error'])) {
+            throw new PhalApi_Exception_BadRequest(T('miss upload file: {file}', array('file' => $index)));
         }
 
         if ($_FILES[$index]['error'] != UPLOAD_ERR_OK) {
-            throw new PhalApi_Exception_BadRequest(T ('fail to upload file with error = {error}', array ('error' => $_FILES[$index]['error'])));
+            throw new PhalApi_Exception_BadRequest(T('fail to upload file with error = {error}', array('error' => $_FILES[$index]['error'])));
         }
 
         $sizeRule         = $rule;
         $sizeRule['name'] = $sizeRule['name'] . '.size';
-        $this->filterByRange ($_FILES[$index]['size'], $sizeRule);
+        $this->filterByRange($_FILES[$index]['size'], $sizeRule);
 
-        if (!empty($rule['range']) && is_array ($rule['range'])) {
-            $rule['range'] = array_map ('strtolower', $rule['range']);
-            $this->formatEnumValue (strtolower ($_FILES[$index]['type']), $rule);
+        if (!empty($rule['range']) && is_array($rule['range'])) {
+            $rule['range'] = array_map('strtolower', $rule['range']);
+            $this->formatEnumValue(strtolower($_FILES[$index]['type']), $rule);
         }
 
         //对于文件后缀进行验证
         if (!empty($rule['ext'])) {
-            $ext = trim (strrchr ($_FILES[$index]['name'], '.'), '.');
-            if (is_string ($rule['ext'])) {
-                $rule['ext'] = explode (',', $rule['ext']);
+            $ext = trim(strrchr($_FILES[$index]['name'], '.'), '.');
+            if (is_string($rule['ext'])) {
+                $rule['ext'] = explode(',', $rule['ext']);
             }
             if (!$ext) {
-                throw new PhalApi_Exception_BadRequest(T ('Not the file type {ext}', array ('ext' => json_encode ($rule['ext']))));
+                throw new PhalApi_Exception_BadRequest(T('Not the file type {ext}', array('ext' => json_encode($rule['ext']))));
             }
-            if (is_array ($rule['ext'])) {
-                $rule['ext'] = array_map ('strtolower', $rule['ext']);
-                if (!in_array (strtolower ($ext), $rule['ext'])) {
-                    throw new PhalApi_Exception_BadRequest(T ('Not the file type {ext}', array ('ext' => json_encode ($rule['ext']))));
+            if (is_array($rule['ext'])) {
+                $rule['ext'] = array_map('strtolower', $rule['ext']);
+                if (!in_array(strtolower($ext), $rule['ext'])) {
+                    throw new PhalApi_Exception_BadRequest(T('Not the file type {ext}', array('ext' => json_encode($rule['ext']))));
                 }
             }
         }
