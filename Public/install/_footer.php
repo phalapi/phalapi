22839d75448f6fa9d55093d54859baf61a7507c3
dogstar <chanzonghuang@gmail.com>
2017-02-16 23:32:01 +0800

    2016 -> 2017

--- Public/install/_footer.php
+++ Public/install/_footer.php
@@ -1,7 +1,7 @@
 <?php defined('PHALAPI_INSTALL') || die('no access'); ?>
 
       <footer class="footer margin-top">
-        <p class="text-white text-little">&copy;2015-2016 <a class="text-yellow text-little" href="http://www.phalapi.net/" >PhalApi</a> All Rights Reserved. 粤ICP备15028808号</p>
+        <p class="text-white text-little">&copy;2015-2017 <a class="text-yellow text-little" href="http://www.phalapi.net/" >PhalApi</a> All Rights Reserved. 粤ICP备15028808号</p>
       </footer>
 
     </div> <!-- /container -->
