95f2f701006a6850a5ac7739d26c020f24f3f483
dogstar <chanzonghuang@gmail.com>
2017-03-19 17:17:48 +0800

    just tab

--- Demo/Api/Default.php
+++ Demo/Api/Default.php
@@ -7,27 +7,27 @@
 
 class Api_Default extends PhalApi_Api {
 
-	public function getRules() {
+    public function getRules() {
         return array(
             'index' => array(
                 'username' 	=> array('name' => 'username', 'default' => 'PHPer', ),
             ),
         );
-	}
-	
-	/**
-	 * Default API service
-	 * @return string title title
-	 * @return string content content
-	 * @return string version versio with format: X.X.X
-	 * @return int time current timestamp
-	 */
-	public function index() {
+    }
+
+    /**
+     * Default API service
+     * @return string title title
+     * @return string content content
+     * @return string version versio with format: X.X.X
+     * @return int time current timestamp
+     */
+    public function index() {
         return array(
             'title' => 'Hello World!',
             'content' => T('Hi {name}, welcome to use PhalApi!', array('name' => $this->username)),
             'version' => PHALAPI_VERSION,
             'time' => $_SERVER['REQUEST_TIME'],
         );
-	}
+    }
 }
