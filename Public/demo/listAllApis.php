f495fc3d1fc7517daaa9eb43e95a0bf5e51c6371
秋寒 <442484396@qq.com>
2017-05-16 20:15:46 +0800

    listAllApis.php添加提示

--- Public/demo/listAllApis.php
+++ Public/demo/listAllApis.php
@@ -261,5 +261,11 @@ $table_color_arr = explode(" ", "red orange yellow olive teal blue violet purple
 if ($env){
     $string = ob_get_clean ();
     saveHtml ( 'index', $string);
+    $str = "脚本执行完毕!文档保存路径为:";
+    if (strtoupper ( substr ( PHP_OS, 0,3)) == 'WIN'){
+        $str = iconv ( 'utf-8', 'gbk', $str);
+    }
+    $str .= $root . DIRECTORY_SEPARATOR . 'doc' ;
+    echo $str;
+    exit(0);
 }
-?>
