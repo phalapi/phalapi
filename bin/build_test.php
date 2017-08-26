2edfb31267a125bb3bd96db8943590f892f11d5f
dogstar <chanzonghuang@gmail.com>
2017-08-25 23:57:24 -0700

    fixed comments

--- bin/build_test.php
+++ bin/build_test.php
@@ -132,10 +132,10 @@ class PhpUnderControl_" . str_replace(array('_', '\\'), array('', ''), $classNam
     protected function tearDown()
     {
         // 输出本次单元测试所执行的SQL语句
-        // var_dump(DI()->tracer->getSqls());
+        // var_dump(\PhalApi\DI()->tracer->getSqls());
 
         // 输出本次单元测试所涉及的追踪埋点
-        // var_dump(DI()->tracer->getSqls());
+        // var_dump(\PhalApi\DI()->tracer->getSqls());
     }
 
 ";
