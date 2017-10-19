cb1ec0a8f4484c8c7ebd0ff17b44fecf7d5d8460
dogstar <chanzonghuang@gmail.com>
2017-10-19 22:18:18 +0800

    去掉兼容代码，更为简洁

--- config/di.php
+++ config/di.php
@@ -4,7 +4,7 @@
  * 
  * @license     http://www.phalapi.net/license GPL 协议
  * @link        http://www.phalapi.net/
- * @author dogstar <chanzonghuang@gmail.com> 2017-07-13
+ * @author      dogstar <chanzonghuang@gmail.com> 2017-07-13
  */
 
 use PhalApi\Loader;
@@ -13,15 +13,9 @@ use PhalApi\Logger;
 use PhalApi\Logger\FileLogger;
 use PhalApi\Database\NotORMDatabase;
 
-/** ---------------- 注册&初始化 基本服务组件 ---------------- **/
-
-// 兼容PhalApi 1.x 旧版本
-$loader = new Loader(API_ROOT, array('Library', 'library'));
+/** ---------------- 基本注册 必要服务组件 ---------------- **/
 
 $di = \PhalApi\DI();
-
-// 自动加载
-$di->loader = $loader;
 
 // 配置
 $di->config = new FileConfig(API_ROOT . '/config');
@@ -37,22 +31,16 @@ $di->notorm = new NotORMDatabase($di->config->get('dbs'), $di->debug);
 
 /** ---------------- 定制注册 可选服务组件 ---------------- **/
 
-/**
 // 签名验证服务
-$di->filter = new \PhalApi\Filter\SimpleMD5Filter();
- */
+// $di->filter = new \PhalApi\Filter\SimpleMD5Filter();
 
-/**
 // 缓存 - Memcache/Memcached
-$di->cache = function () {
-    return new \PhalApi\Cache\MemcacheCache(DI()->config->get('sys.mc'));
-};
- */
+// $di->cache = function () {
+//     return new \PhalApi\Cache\MemcacheCache(DI()->config->get('sys.mc'));
+// };
 
-/**
 // 支持JsonP的返回
-if (!empty($_GET['callback'])) {
-    $di->response = new \PhalApi\Response\JsonpResponse($_GET['callback']);
-}
- */
+// if (!empty($_GET['callback'])) {
+//     $di->response = new \PhalApi\Response\JsonpResponse($_GET['callback']);
+// }
 
