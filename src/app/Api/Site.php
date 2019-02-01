6a49260fd87118563ca91cf0d50c501a196ae5cc
dogstar <411170161@qq.com>
2019-02-01 11:30:49 +0800

    格式化代码

--- src/app/Api/Site.php
+++ src/app/Api/Site.php
@@ -1,38 +1,34 @@
 <?php
 namespace App\Api;
-
 use PhalApi\Api;
 
 /**
  * 默认接口服务类
- *
  * @author: dogstar <chanzonghuang@gmail.com> 2014-10-04
  */
-
 class Site extends Api {
-
-	public function getRules() {
+    public function getRules() {
         return array(
             'index' => array(
-                'username' 	=> array('name' => 'username', 'default' => 'PhalApi', 'desc' => '用户名'),
+                'username'  => array('name' => 'username', 'default' => 'PhalApi', 'desc' => '用户名'),
             ),
         );
-	}
-	
-	/**
-	 * 默认接口服务
+    }
+
+    /**
+     * 默认接口服务
      * @desc 默认接口服务，当未指定接口服务时执行此接口服务
-	 * @return string title 标题
-	 * @return string content 内容
-	 * @return string version 版本，格式：X.X.X
-	 * @return int time 当前时间戳
+     * @return string title 标题
+     * @return string content 内容
+     * @return string version 版本，格式：X.X.X
+     * @return int time 当前时间戳
      * @exception 400 非法请求，参数传递错误
-	 */
-	public function index() {
+     */
+    public function index() {
         return array(
             'title' => 'Hello ' . $this->username,
             'version' => PHALAPI_VERSION,
             'time' => $_SERVER['REQUEST_TIME'],
         );
-	}
+    }
 }
