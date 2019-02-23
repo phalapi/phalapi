ca6ee9809b0b0d35b4126f3ad4c450988106be65
QinHaolei <haoleiqin@qq.com>
2019-02-23 15:33:19 +0800

    增加默认Redis缓存配置

--- config/sys.php
+++ config/sys.php
@@ -1,30 +1,38 @@
-<?php 
+<?php
 /**
  * 以下配置为系统级的配置，通常放置不同环境下的不同配置
- * 
+ *
  * @license     http://www.phalapi.net/license GPL 协议
  * @link        http://www.phalapi.net/
  * @author dogstar <chanzonghuang@gmail.com> 2017-07-13
  */
 
 return array(
-	/**
-	 * 默认环境配置
-	 */
-	'debug' => false,
+    /**
+     * 默认环境配置
+     */
+    'debug' => false,
 
-	/**
-	 * MC缓存服务器参考配置
-	 */
-	 'mc' => array(
+    /**
+     * MC缓存服务器参考配置
+     */
+    'mc' => array(
         'host' => '127.0.0.1',
         'port' => 11211,
-	 ),
+    ),
 
+    /**
+     * Redis缓存服务器参考配置
+     */
+    'redis' => array(
+        'host' => '127.0.0.1',
+        'port' => 6379,
+	),
+	
     /**
      * 加密
      */
     'crypt' => array(
-        'mcrypt_iv' => '12345678',      //8位
+        'mcrypt_iv' => '12345678', //8位
     ),
 );
