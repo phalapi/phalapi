d3c8dddb8e5cd8fc797cfb87a959b8929876a2a0
dogstar <chanzonghuang@gmail.com>
2020-03-09 16:51:07 +0800

    up

--- public/portal/js/lay-module/layuimini/miniTheme.js
+++ public/portal/js/lay-module/layuimini/miniTheme.js
@@ -194,9 +194,10 @@ layui.define(["jquery", "layer"], function (exports) {
                     '<ul>\n' + bgColorHtml + '</ul>\n' +
                     '</div>\n' +
                     '<div class="more-menu-list">\n' +
-                    '<a class="more-menu-item" href="http://layuimini.99php.cn/docs/" target="_blank"><i class="layui-icon layui-icon-read" style="font-size: 19px;"></i> 开发文档</a>\n' +
-                    '<a class="more-menu-item" href="https://github.com/zhongshaofa/layuimini" target="_blank"><i class="layui-icon layui-icon-tabs" style="font-size: 16px;"></i> 开源地址</a>\n' +
-                    '<a class="more-menu-item" href="http://layuimini.99php.cn" target="_blank"><i class="layui-icon layui-icon-theme"></i> 官方网站</a>\n' +
+                    '<a class="more-menu-item" href="http://docs.phalapi.net/#/v2.0/tutorial" target="_blank"><i class="layui-icon layui-icon-read" style="font-size: 19px;"></i> 开发文档</a>\n' +
+                    '<a class="more-menu-item" href="/docs.php" target="_blank"><i class="layui-icon layui-icon-read" style="font-size: 19px;"></i> 在线接口文档</a>\n' +
+                    '<a class="more-menu-item" href="https://github.com/phalapi/phalapi" target="_blank"><i class="layui-icon layui-icon-tabs" style="font-size: 16px;"></i> 开源地址</a>\n' +
+                    '<a class="more-menu-item" href="http://www.phalapi.net" target="_blank"><i class="layui-icon layui-icon-theme"></i> 官方网站</a>\n' +
                     '</div>' +
                     '</div>';
                 layer.open({
