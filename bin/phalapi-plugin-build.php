4491959d12277f581c4b7dfead048b4818afd888
dogstar <chanzonghuang@gmail.com>
2020-05-12 13:29:42 +0800

    fixed

--- bin/phalapi-plugin-build.php
+++ bin/phalapi-plugin-build.php
@@ -73,6 +73,11 @@ echo realpath($filename), "\n\n";
 function addFileToZip($path, $zipPath, $zip) {
     $handler = opendir($path); //打开当前文件夹由$path指定。
 
+    // fixed: readdir() expects parameter 1 to be resource, boolean given
+    if (!$handler) {
+        return;
+    }
+
     while (($filename = readdir($handler)) !== false) {
         if ($filename != "." && $filename != "..") {//文件夹文件名字为'.'和‘..’，不要对他们进行操作
             if (is_dir($path . "/" . $filename)) {// 如果读取的某个对象是文件夹，则递归
